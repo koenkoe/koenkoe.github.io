742747ef46c07072df63a77175c493bf23a44214
koenkoe <131816470+koenkoe@users.noreply.github.com>
2025-05-07 11:31:03 +0200

    Update request.py

--- request.py
+++ request.py
@@ -6,12 +6,16 @@ website = input("Voer de URL van de website in (voorbeeld.com): ")
 url = ("https://" + website)
 
 try:
-
     response = requests.get(url)
     if response.status_code == 200:
         print("Website succesvol opgehaald.")
+        with open("pagina.html", "w", encoding="utf-8") as f:
+            f.write(response.text)
+        print("Webpagina opgeslagen als 'pagina.html'.")
+        print("Bestand opgeslagen in:", os.path.abspath("pagina.html"))
+
     zoekwoord = input("Welk woord wil je zoeken op de website?: ")
-    if zoekwoord in response.text:
+    if zoekwoord.lower() in response.text.lower():
         print("Het woord", zoekwoord , "staat op https://" + website )
     else:
         print("Het woord" , zoekwoord , "staat niet op https://" + website )
